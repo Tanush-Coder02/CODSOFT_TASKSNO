b123988f5ed984382a9b0ad005cc5081c031132f
Tanush-Coder02 <tanushpal123@gmailcom>
2026-08-11 19:36:35 +0530

    Add Task 4: Rock-Paper-Scissors Game

--- TASK4_RockPaperScissors/rock_paper_scissors.py
+++ TASK4_RockPaperScissors/rock_paper_scissors.py
@@ -1,40 +1,51 @@
 import random
+
 def get_computer_choice():
-    return random.choice(['Rock' , 'Paper' , 'Scissors'])
-def determine_winner(user , computer):
-     if user == computer:
+    return random.choice(['rock', 'paper', 'scissors'])
+
+def determine_winner(user, computer):
+    if user == computer:
         return "tie"
-     elif (user == 'rock' and computer == 'scissors') or \
+    elif (user == 'rock' and computer == 'scissors') or \
          (user == 'scissors' and computer == 'paper') or \
          (user == 'paper' and computer == 'rock'):
         return "user"
-     else:
+    else:
         return "computer"
+
 def play_game():
-    print("Rock , Paper , Scissors")
+    print("Rock, Paper, Scissors!")
     user_score = 0
     computer_score = 0
+
     while True:
-        user_choice = input("ENTER 'Rock' , 'Paper' , 'Scissors'")
+        user_choice = input("Enter rock, paper, or scissors: ").lower()
 
-        if user_choice not in ['Rock', 'Paper', 'Scissors']:
-         print("Invalid choice. Try again.")
-        continue      
-computer_choice = get_computer_choice()
+        if user_choice not in ['rock', 'paper', 'scissors']:
+            print("Invalid choice. Try again.")
+            continue
 
-if winner == "tie":
+        computer_choice = get_computer_choice()
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+
+        winner = determine_winner(user_choice, computer_choice)
+
+        if winner == "tie":
             print("It's a tie!")
-elif winner == "user":
+        elif winner == "user":
             print("You win this round!")
             user_score += 1
-else:
-        print("Computer wins this round!")
-        computer_score += 1
-            
-play_again = input("Play again? (y/n): ").lower()
-if play_again != 'y':
-        print("Thanks for playing!")
+        else:
+            print("Computer wins this round!")
+            computer_score += 1
 
-        break
+        print(f"Score -> You: {user_score} | Computer: {computer_score}")
+
+        play_again = input("Play again? (y/n): ").lower()
+        if play_again != 'y':
+            print("Thanks for playing!")
+            break
+
 if __name__ == "__main__":
-          play_game()
+    play_game()
